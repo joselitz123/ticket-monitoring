a681ea971bfd737f6abb2d5dfc835b29e58895c9
joselitz123 <joselitophala@gmail.com>
2019-04-10 10:32:20 +0800

    Made the fadeout transtion work in the ticket notification board

--- src/client/home/notificationBoard/actionList.js
+++ src/client/home/notificationBoard/actionList.js
@@ -2,30 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setSelectedNotifID, toggleDismissConfirmationPromp } from '../../actions/home/ticketNotif/actions';
 import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const ActionList = ({action_list, setSelectedNotifID, toggleDismissConfirmationPromp, notif_id, dismiss_animation}) => {
+const ActionList = ({ action_list, setSelectedNotifID, toggleDismissConfirmationPromp, notif_id, dismiss_animation }) => {
 
     const dismissPrompHandler = id => {
-        
+
         setSelectedNotifID(id);
 
         toggleDismissConfirmationPromp(true);
 
     }
 
-    return action_list.map(list => (
-        <tr key={list.notif_id} className={`animated ${list.notif_id == notif_id && dismiss_animation ? 'fadeOutRight' : '' }`} >     
-            <td>{list.notif}</td>            
-            <td className="options" style={{"display": "flex"}} >
-                <a href="#" data-toggle="tooltip" title="Dismiss" onClick={()=>dismissPrompHandler(list.notif_id)} style={{"margin": "0px 5px"}} >
-                    <i className="material-icons">clear</i>
-                </a>
-                <a  href="#" onClick={()=>setSelectedNotifID(list.notif_id)} style={{"margin": "0px 5px"}} >
-                    <i className="material-icons">snooze</i>
-                </a>
-            </td>
-        </tr>
-    ))
+    return (
+        <TransitionGroup component={null}>
+            {action_list.map(list => (
+                <CSSTransition timeout={500} key={list.notif_id} classNames="item">
+                    <tr >
+                        <td>{list.notif}</td>
+                        <td className="options" style={{ "display": "flex" }} >
+                            <a href="#" data-toggle="tooltip" title="Dismiss" onClick={() => dismissPrompHandler(list.notif_id)} style={{ "margin": "0px 5px" }} >
+                                <i className="material-icons">clear</i>
+                            </a>
+                            <a href="#" onClick={() => setSelectedNotifID(list.notif_id)} style={{ "margin": "0px 5px" }} >
+                                <i className="material-icons">snooze</i>
+                            </a>
+                        </td>
+                    </tr>
+                </CSSTransition>
+            ))}
+        </TransitionGroup>
+    )
 }
 
 
--- src/client/home/notificationBoard/notifList.js
+++ src/client/home/notificationBoard/notifList.js
@@ -5,10 +5,10 @@ import ActionList from './actionList';
 import { showOptions } from '../../actions/home/optExtension/ticketNotif/actions';
 import { setSelectedTicketID } from '../../actions/home/ticketNotif/actions';
 
-const NotifList = ({data, showOptions, setSelectedTicketID}) =>{
+const NotifList = ({ data, showOptions, setSelectedTicketID }) => {
 
     if (data.length == 0) {
-        
+
         return (
             <div className="empty_container"><div>No data can be displayed yet.</div></div>
         );
@@ -23,33 +23,33 @@ const NotifList = ({data, showOptions, setSelectedTicketID}) =>{
         });
     }
 
-    return data.map(data => 
+    return data.map(data =>
         (
-           <div key={data._id} className={`card priority_${data.ticket_details.priority.charAt(0)}`}  >
-               <div className="card-header" id="headingOne">
-               <h2 className="mb-0">
-                   <button className="btn btn-link" type="button" onContextMenu={e=>optExtension(e, data._id)} data-toggle="collapse" data-target={`#${data._id}`} aria-expanded="false" aria-controls={data._id} onClick={()=>setSelectedTicketID(data._id)} >
-                   {`${data._id}: ${data.ticket_details.shrt_desc}`}
-                   </button>
-               </h2>
-               </div>
+            <div key={data._id} className={`card priority_${data.ticket_details.priority.charAt(0)}`}  >
+                <div className="card-header" id="headingOne">
+                    <h2 className="mb-0">
+                        <button className="btn btn-link" type="button" onContextMenu={e => optExtension(e, data._id)} data-toggle="collapse" data-target={`#${data._id}`} aria-expanded="false" aria-controls={data._id} onClick={() => setSelectedTicketID(data._id)} >
+                            {`${data._id}: ${data.ticket_details.shrt_desc}`}
+                        </button>
+                    </h2>
+                </div>
 
-               <div id={data._id} className="collapse" aria-labelledby="headingOne" data-parent="#ticketNotifAcc">
-               <div className="card-body">
-                   <div className="tab-content">
-                       <div className="tab-pane active" id="profile">
-                           <table className="table">
-                               <tbody>
-                                   <ActionList action_list={data.notifications} />
-                               </tbody>
-                           </table>
-                       </div>
-                   </div>
-               </div>
-               </div>
-           </div>
-       )
-   )
+                <div id={data._id} className="collapse" aria-labelledby="headingOne" data-parent="#ticketNotifAcc">
+                    <div className="card-body">
+                        <div className="tab-content">
+                            <div className="tab-pane active" id="profile">
+                                <table className="table">
+                                    <tbody>
+                                        <ActionList action_list={data.notifications} />
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    )
 };
 
 NotifList.propTypes = {
